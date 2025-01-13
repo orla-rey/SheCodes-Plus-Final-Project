25146022b9c41c2383c8b8b06a6f7fe562149878
orla-rey <orla.o.reynolds@gmail.com>
2025-01-13 10:31:11 -0500

    added api & javascript for city & temperature

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#weather-app-temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#weather-app-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "1c21507bfe367ft66ea583e00ofa422f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
-event.preventDefault();
-let searchInput = document.querySelector("#search-form-input");
-let cityElement = document.querySelector("#weather-app-city");
-cityElement.innerHTML = searchInput.value
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit); 
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Montreal")
