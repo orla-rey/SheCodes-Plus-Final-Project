1d7b959d62da73039de9afefe86a044ba1e88bbc
orla-rey <orla.o.reynolds@gmail.com>
2025-01-15 10:41:27 -0500

    updated forecast HTML & CSS. added forecast javascript

--- src/index.js
+++ src/index.js
@@ -52,7 +52,38 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast (){
+let forecastElement = document.querySelector("#forecast");
+
+let days = ['Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+let forecastHtml = "";
+
+days.forEach(function(day){
+forecastHtml = forecastHtml + `
+  <div class="forecast-daily" id="forecast-daily">
+    <div class="forecast-date" id="forecast-date">
+      ${day}
+    </div>
+    <div class="forecast-icon" id="forecast-icon">
+      🌧️
+    </div>
+    <div class="forecast-temperatures" id="forecast-temperatures">
+      <div class="forecast-temperature" id="forecast-temperature">
+        <strong>19°</strong>
+      </div>
+      <div class="forecast-temperature" id="forecast-temperature">
+        23°
+      </div>
+    </div>
+  </div>
+`;
+});
+
+forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Montreal");
+displayForecast();
